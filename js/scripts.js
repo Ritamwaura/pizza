2a2309bf773e9bc5cbf35f07e6a6e2f6c405611a
Your Name <mwaurarita2019@gmail.com>
2019-07-29 19:08:21 +0300

    save the newly corrected space

--- js/scripts.js
+++ js/scripts.js
@@ -1,81 +1,49 @@
-//business
-function Pizza(size, toppings, crust, total, orderNo) {
-  this.size = size;
-  this.toppings = toppings;
-  this.crust = crust;
-  this.total = total;
-  this.orderNo = orderNo;
-}
-$(document).ready(function(e) {
-
-  $('form#new-pizza').submit(function(event) {
-    event.preventDefault()
-    var sizeOfPizza = $(".size option:selected").val();
-    var toppingsOfPizza = $(".toppings option:selected").val();
-    var crustOfPizza = $(".crust option:selected").val();
-    var total = parseInt(sizeOfPizza) + parseInt(toppingsOfPizza) + parseInt(crustOfPizza);
-    var order = 1;
-    var grandTotal = 0;
-
-    $("table").show();
-    $(".additional-buttons").show();
-    $(".btn.order").hide();
-
-    $("#size").html($(".size option:selected").text() + " - " + sizeOfPizza);
-    $("#toppings").html($(".toppings option:selected").text() + " - " + toppingsOfPizza);
-    $("#crust").html($(".crust option:selected").text() + " - " + crustOfPizza);
-    $("#total").html(total);
-
-    function Pizza(size, toppings, crust, total, orderNo) {
-      this.size = size;
-      this.toppings = toppings;
-      this.crust = crust;
-      this.total = total;
-      this.orderNo = orderNo;
+$(document).ready(function(){
+  $("select#size").change(function(){
+    window.selectsize = $(this).children("option:select").val()
+    if (window.selectsize === "small"){
+      window.money_size = 250;
     }
+    else if (window.selectsize === "big"){
+      window.money_size = 400;
+    }
+    else if (window.selectsize === "large"){
+      window.money_size = 1000;
+    }
+    $("#output1").text(window.selectcrust);
+  })
+  $("select#toppings").change(function(){
+    window.selecttoppings = $(this).children("option:select").val()
+    if (window.selecttoppings === "mush"){
+      window.money_toppings = 70;
+    }
+    else if (window.selecttoppings === "romantic"){
+      window.money_toppings = 70;
+    }
+    else if (window.selecttoppings === "brave"){
+      window.money_toppings = 70
+    }
+    else if (window.selecttoppings === "cheese"){
+      window.money_toppings = 70
+    }
+    $("#output2").text(window.selecttoppings);
+  })
+  $("select#crust").change(function(){
+    window.selectcrust = $(this).children("option:select").val()
+    if (window.selectcrust === "crispy"){
+      window.money_crust = 100;
+    }
+    else if (window.selectcrust === "stuff"){
+      window.money_crust = 150;
+    }
+    else if (window.selectcrust === "gluten-free"){
+      window.money_crust = 200;
+    }
+    $("#output3").text(window.selectcrust);
 
+  })
+  $("#order").click(function(){
 
-    $('.btn.add-pizza').click(function() {
-      var sizeOfPizza = $(".size option:selected").val();
-      var toppingsOfPizza = $(".toppings option:selected").val();
-      var crustOfPizza = $(".crust option:selected").val();
-      var total = parseInt(sizeOfPizza) + parseInt(toppingsOfPizza) + parseInt(crustOfPizza);
-      order = order + 1;
-      grandTotal = grandTotal + total;
+  })
 
-
-      var newPizza = new Pizza(sizeOfPizza, toppingsOfPizza, crustOfPizza, total, order);
-
-      var newRow = '<tr><th scope="row">' + newPizza.orderNo + '</th><td id="size">' + $(".size option:selected").text() + " - " + newPizza.size + '</td><td id="toppings">' + $(".toppings option:selected").text() + " - " + newPizza.toppings + '</td><td id="crust">' + $(".crust option:selected").text() + " - " + newPizza.crust + '</td><td id="total">' + newPizza.total + '</td></tr>'
-
-      $("#pizza").append(newRow);
-    });
-    $(".btn.check-out").click(function() {
-      $(".btn.add-pizza").show();
-      $(".btn.check-out").show();
-      $(".additional-info h3").show();
-      $(".additional-info h3 span").html(grandTotal);
-
-    });
-    $(".btn.yes").click(function() {
-      $(".additional-info h5").hide();
-      $(".btn.yes").hide();
-      $(".btn.no").hide();
-      $(".additional-info .location").show();
-      $(".additional-info h3 span").html(grandTotal + 200);
-    });
-
-    $(".btn.no").click(function() {
-      $(".additional-info h5").hide();
-      $(".btn.yes").hide();
-      $(".btn.no").hide();
-    });
-
-    $(".btn.complete").click(function() {
-      var location = $(".additional-info .location input").val();
-      $(".additional-info h4").show();
-      $(".additional-info .location").hide();
-      $(".additional-info h4 span").html(location);
-    });
-
-  });
+})
